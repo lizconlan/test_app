c27e37631da929358dfcb185287da0ce0acb6b7a
lizconlan <liz@mysociety.org>
2017-06-22 19:58:17 +0100

    Add seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+assemblies = Assembly.create([{ name: 'A Test' }, { name: 'B Test' }])
+
+I18n.with_locale(:es) do
+  assemblies[0].name = '1 Test'
+  assemblies[0].save
+  assemblies[1].name = '0 Test'
+  assemblies[1].save
+end
+
+Part.create(part_number: "42", assemblies: assemblies)
