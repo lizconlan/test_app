2c74926cb8e85986ddc8a277bfb4a417c189fd51
lizconlan <liz@mysociety.org>
2017-06-23 10:26:50 +0100

    Order using with_translations

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Part < ActiveRecord::Base
-  has_and_belongs_to_many :assemblies
+  has_and_belongs_to_many :assemblies, -> {
+    with_translations([I18n.locale]).
+      order('assembly_translations.name asc')
+  }
 end
