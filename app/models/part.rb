6cc22a441bbe585f03e15c82e6f795b3d8722920
lizconlan <liz@mysociety.org>
2017-06-23 10:27:44 +0100

    Use joins with the order clause

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,7 @@
 # -*- encoding : utf-8 -*-
 class Part < ActiveRecord::Base
-  has_and_belongs_to_many :assemblies
+  has_and_belongs_to_many :assemblies, -> {
+    joins(:translations).
+      order('assembly_translations.name asc')
+  }
 end
