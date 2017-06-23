e71941e267f7eccc5ab61cdb0ee98d8c642d6be1
lizconlan <liz@mysociety.org>
2017-06-23 14:04:09 +0100

    Order by using includes inside a with_locale block

--- app/models/part.rb
+++ app/models/part.rb
@@ -1,4 +1,9 @@
 # -*- encoding : utf-8 -*-
 class Part < ActiveRecord::Base
-  has_and_belongs_to_many :assemblies
+  has_and_belongs_to_many :assemblies, -> {
+    I18n.with_locale(I18n.locale) do
+      includes(:translations).
+        order('assembly_translations.name asc')
+    end
+  }
 end
